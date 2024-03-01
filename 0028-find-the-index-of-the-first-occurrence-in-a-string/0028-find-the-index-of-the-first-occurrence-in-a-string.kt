67c661e389c901922b2c60dbd147935f40695c10
sukkyu.oh <scarfunk@naver.com>
2024-03-01 10:50:11 +0900

    Time: 220 ms (5.07%), Space: 36.8 MB (6.18%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun strStr(haystack: String, needle: String): Int {
+        var ptr1 = 0
+        var i = 0
+        while(i < haystack.length) {
+            println("$i : $ptr1 : ${haystack[i]}")
+            if(needle[ptr1] == haystack[i]) {
+                ptr1++
+                if(ptr1 == needle.length) return i - needle.length + 1
+            } else {
+                // reset
+                i = i - ptr1
+                ptr1 = 0
+            }
+            i++
+        }
+        return - 1
+    }
+}
